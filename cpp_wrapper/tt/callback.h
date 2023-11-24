ded2f36040c8e6e8156c739b2e824b2cb59838c9
cncsmonster <cncsmonster@163.com>
2023-11-24 22:45:29 +0800

    tmp:complete wrapp tool

--- cpp_wrapper/tt/callback.h
+++ cpp_wrapper/tt/callback.h
@@ -1,6 +1,12 @@
 #ifndef _CALLBACK_H_
 #define _CALLBACK_H_
 
+// 为了给cpp代码调用
+
+#ifdef __cplusplus
+extern "C" {
+#endif // DEBUG
+
 typedef void (*CB_S)(char *);
 typedef void (*CB_I_S)(int,char *);
 typedef void (*CB_S_I_S_S)(char *,int,char *,char *);
@@ -11,6 +17,10 @@ void call_cb_s(CB_S cb);
 void call_cb_i_s(CB_I_S cb);
 void call_cb_s_i_s_s(CB_S_I_S_S cb);
 void call_cb_s_i_s_s_i(CB_S_I_S_S_I cb);
+
+#ifdef __cplusplus
+}
+#endif // DEBUG
 
 #endif // !_
 
--- cpp_wrapper/tt/run_test.sh
+++ cpp_wrapper/tt/run_test.sh
@@ -1,2 +1,7 @@
+# test callback lib
 gcc -o test_callback test_callback.c ./callback.so 
 ./test_callback
+
+# test wrapp demo
+g++ -o test_wrapp_demo wrapp_demo.cpp ./callback.so -std=c++11
+./test_wrapp_demo
